122f62897966d3d0257b53b5e721e961ba3a2886
Chayut Wanit <jormzazajormmily0804@gmail.com>
2023-10-24 21:57:32 +0700

    Version 1.01
    -Google Provider Login Api
    -Link Test Page

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -1,5 +1,7 @@
 import type { NextAuthOptions } from "next-auth";
 import LineProvider from "next-auth/providers/line";
+import TwitterProvider from "next-auth/providers/twitter";
+import GoogleProvider from "next-auth/providers/google";
 
 export const options: NextAuthOptions = {
   providers: [
@@ -7,6 +9,15 @@ export const options: NextAuthOptions = {
       authorization: { params: { scope: "openid profile email" } },
       clientId: process.env.LINE_CLIENT_ID as string,
       clientSecret: process.env.LINE_CLIENT_SECRET as string,
+    }),
+    TwitterProvider({
+      clientId: process.env.TWITTER_CLIENT_ID as string,
+      clientSecret: process.env.TWITTER_CLIENT_SECRET as string,
+      version: "2.0"
+    }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string
     })
   ]
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Noto_Sans_Thai } from 'next/font/google'
 import '@/styles/globals.css'
 import { Provider } from '@/app/provider';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Noto_Sans_Thai({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Create Next App',
