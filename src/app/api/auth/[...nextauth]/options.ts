d4e1aeaccc6a1766b0dff2fc5c7f1b0c3059c4ee
Chayut Wanit <jormzazajormmily0804@gmail.com>
2023-10-25 20:58:31 +0700

    NextJs Version 1.01
    -Firebase Adaptor
    -Google account provider

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -3,6 +3,9 @@ import LineProvider from "next-auth/providers/line";
 import TwitterProvider from "next-auth/providers/twitter";
 import GoogleProvider from "next-auth/providers/google";
 
+import { FirestoreAdapter } from "@auth/firebase-adapter"
+import { cert } from "firebase-admin/app"
+
 export const options: NextAuthOptions = {
   providers: [
     LineProvider({
@@ -19,5 +22,12 @@ export const options: NextAuthOptions = {
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string
     })
-  ]
+  ],
+  adapter: FirestoreAdapter({
+    credential: cert({
+      projectId: process.env.FIREBASE_PROJECT_ID as string,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL as string,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY!.replace(/\\n/g, '\n') as string,
+    }),
+  }),
 }
