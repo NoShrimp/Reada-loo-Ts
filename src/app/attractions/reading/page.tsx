ed91ac4502008c6c6d34cd49ce67c4cfc78b15d3
Chayut Wanit <jormzazajormmily0804@gmail.com>
2023-11-25 19:29:23 +0700

    version 2.00
    -front end merge
    -pdf function

--- src/app/attractions/reading/page.tsx
+++ src/app/attractions/reading/page.tsx
@@ -1,8 +1,44 @@
+'use client'
 import React from 'react'
 import { FaHome } from "react-icons/fa";
+import { useCallback, useState } from 'react'; // นำเข้าฟังก์ชัน useCallback และ useState จาก React
+import { useResizeObserver } from '@wojtekmaj/react-hooks'; // นำเข้า hook useResizeObserver จาก @wojtekmaj/react-hooks
+import { pdfjs, Document, Page } from 'react-pdf'; // นำเข้า pdfjs, Document, Page จาก react-pdf
+import 'react-pdf/dist/esm/Page/AnnotationLayer.css'; // นำเข้า CSS สำหรับ AnnotationLayer
+import 'react-pdf/dist/esm/Page/TextLayer.css'; // นำเข้า CSS สำหรับ TextLayer
+// import '@/styles/Sample.css'; // นำเข้า CSS สำหรับ component นี้
+import type { PDFDocumentProxy } from 'pdfjs-dist'; // นำเข้า type PDFDocumentProxy จาก pdfjs-dist
+
+pdfjs.GlobalWorkerOptions.workerSrc = new URL( // กำหนดตำแหน่งของ worker script สำหรับ pdfjs
+    'pdfjs-dist/build/pdf.worker.min.js',
+    import.meta.url,
+).toString();
+
+const options = { // กำหนด options สำหรับ react-pdf
+    cMapUrl: '/cmaps/',
+    standardFontDataUrl: '/standard_fonts/',
+};
+
+const resizeObserverOptions = {}; // กำหนด options สำหรับ useResizeObserver
+const maxWidth = 800; // กำหนดความกว้างสูงสุดของหน้า PDF
 
 
 export default function Reading() {
+    
+    const [numPages, setNumPages] = useState<number>(); // กำหนด state สำหรับ numPages
+    const [containerRef, setContainerRef] = useState<HTMLElement | null>(null); // กำหนด state สำหรับ containerRef
+    const [containerWidth, setContainerWidth] = useState<number>(); // กำหนด state สำหรับ containerWidth
+    const onResize = useCallback<ResizeObserverCallback>((entries) => { // สร้าง callback function สำหรับการจัดการเมื่อขนาดของ container เปลี่ยนแปลง
+        const [entry] = entries;
+        if (entry) {
+            setContainerWidth(entry.contentRect.width); // กำหนดความกว้างของ container
+        }
+    }, []);
+    useResizeObserver(containerRef, resizeObserverOptions, onResize); // ใช้ useResizeObserver สำหรับการตรวจสอบการเปลี่ยนแปลงขนาดของ container
+    function onDocumentLoadSuccess({ numPages: nextNumPages }: PDFDocumentProxy): void { // สร้าง function สำหรับการจัดการเมื่อเอกสาร PDF โหลดสำเร็จ
+        setNumPages(nextNumPages); // กำหนดจำนวนหน้าของ PDF
+    }
+
     return (
         <div className="bg-neutral">
             <section className="container max-w-6xl">
@@ -36,11 +72,24 @@ export default function Reading() {
             </div>
             </div>
             </div>
-            <div className="p-8 relative flex flex-col justify-center overflow-hidden bg-base-100 bg-gradient-to-tr from-pink-500 bg-[length:100%_calc(100%-100vh+5px)] bg-no-repeat py-6 [--tw-gradient-to:rgb(255_255_255)50%] [--tw-gradient-from:rgb(236_72_153)50%] after:fixed after:inset-0 after:top-1.5 after:-z-10 after:bg-white sm:py-12">
-                <p className="text-4xl font-bold py-10">Scroll to see effect</p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
-                aliqua.
+            <div className="bg-white text-white dark:bg-black">
+            <div className="flex flex-col items-center my-2 py-2">
+                <div>
+                </div>
+                <div className="w-full max-w-full mx-2 my-4" ref={setContainerRef}>
+                    <Document file={'/pdf/ERD.pdf'} onLoadSuccess={onDocumentLoadSuccess} options={options} className="flex flex-col items-center">
+                        {Array.from(new Array(numPages), (el, index) => (
+                            <Page
+                                key={`page_${index + 1}`} // กำหนด key สำหรับแต่ละหน้า
+                                pageNumber={index + 1} // กำหนดหมายเลขหน้า
+                                width={containerWidth ? Math.min(containerWidth, maxWidth) : maxWidth} // กำหนดความกว้างของหน้า โดยใช้ความกว้างของ container หรือความกว้างสูงสุด ขึ้นอยู่กับค่าที่น้อยกว่า
+                                className="my-2 shadow-md"
+                            />
+                        ))}
+                    </Document>
+                </div>
             </div>
+        </div>
             </section>
         </div>
     )
